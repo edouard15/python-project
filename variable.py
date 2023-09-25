ef8c1399098f75abb19bbdbdf4c077b883b52a15
Edouard <edouardakotonou@gmail.com>
2023-09-25 17:45:43 -0500

    chore Break: python first commit Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET> Problem Solution Note

---
+++ variable.py
@@ -0,0 +1,17 @@
+box = "chocolate cake" #string data
+print (box)
+
+number = 10 #number data
+print (number)
+
+rain = True #boolean data
+print (rain)
+iLoveMyWife = "happy birthday"
+print (iLoveMyWife)
+
+#we can't start a variable with a number (2rain).
+# we can't have hyphens in the middle of variable (my-box).
+# we can't have space in the variable (my box).
+# Convention:
+# myBox = "cake" or my_box = "cake" 
+#myBox = "cake"
